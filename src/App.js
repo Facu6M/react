9965c07ea77d7d6fea4b7c9e0c33a1d71df54a2d
Facu6M <facu6p@gmail.com>
2022-07-06 13:07:41 -0300

    se agrego

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import logo from './images/FreeCodeCamp_logo.svg.png';
+
+// components
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+
+// hooks
+import { useState } from 'react';
+
+function App() {
+
+const [numClics, setNumClics ] = useState(0);
+
+ const manejarClic = () => {
+  setNumClics(numClics + 1);
+ }
+
+const reiniciarContador = () => {
+  setNumClics(0);
+}
+
+  return (
+    <div className="App">
+<div className='freecodecamp-logo-contenedor'>
+<img
+ className='freecodecamp-logo'
+ src={logo}
+/>
+</div>
+<div className='contenedor-principal'>
+<Contador
+numClics={numClics} />
+<Boton
+texto='Clic'
+esBotonDeClic={true}
+manejarClic={manejarClic}/>
+<Boton
+ texto='Reiniciar'
+ esBotonDeClic={false}
+ manejarClic={reiniciarContador}/>
+</div>
+    </div>
+  );
+}
+
+export default App;
